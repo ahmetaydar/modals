be00a3180079359736238d1b8db9c09a6a020da8
ahmetaydar <a.aydar2014@gmail.com>
2022-11-08 13:49:36 +0300

    added button

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelector('.show-modal');
+console.log(btnsOpenModel);
