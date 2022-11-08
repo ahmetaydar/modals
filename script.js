0ce9d3537733359fbe40a7c18e9198a53d03a97f
ahmetaydar <a.aydar2014@gmail.com>
2022-11-08 15:29:15 +0300

    last commit

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btn = document.querySelector('.close-modal');
-const btnsOpenModel = document.querySelector('.show-modal');
-console.log(btnsOpenModel);
+const btnCloseModel = document.querySelector('.close-modal');
+const btnsOpenModel = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModel.length; i++)
+  btnsOpenModel[i].addEventListener('click', openModal);
+
+btnCloseModel.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
